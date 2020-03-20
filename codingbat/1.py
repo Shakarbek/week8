a89c8606fb2f0ddfac930483affa02a3c28d7660
Shakarbek <shakarbek.a.b@gmail.com>
2020-03-20 12:50:57 +0600

    first commit

---
+++ codingbat/1.py
@@ -0,0 +1,10 @@
+def sleep_in(weekday, vacation):
+    if weekday==False or vacation==True:
+        print("True")
+        return True
+    else:
+        print("False")
+        return False
+
+sleep_in(False, False)
+sleep_in(True, False)
---
+++ codingbat/3.py
@@ -0,0 +1,8 @@
+def sum_double(a,b):
+    if a!=b:
+        return a+b
+    else:
+        print(2*(a+b))
+        return 2*(a+b)
+
+sum_double(2,2)
